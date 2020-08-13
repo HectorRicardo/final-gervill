2ee1adae3933ea5392d65d0884fa608fb912b9de
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-08-13 10:11:57 -0500

    First commit

---
+++ src/own/main/Main.java
@@ -0,0 +1,48 @@
+package own.main;
+
+import gervill.com.sun.media.sound.DLSSoundbank;
+import gervill.com.sun.media.sound.SF2Soundbank;
+import gervill.javax.sound.midi.Soundbank;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // Step 1. Set up synthesizer
+        final SynthesizerPlayer player = new SynthesizerPlayer();
+
+        // Step 2. Repeat operations
+        openPlayAndClose(player, new SF2Soundbank(new File("assets/gm.sf2")));
+        openPlayAndClose(player, new DLSSoundbank(new File("assets/gm.dls")));
+        openPlayAndClose(player, null);
+    }
+
+    public static void openPlayAndClose(SynthesizerPlayer player, Soundbank soundbank) {
+        // Open synthesizer
+        player.open(soundbank);
+
+        // Play something
+        if (player.readyToStartPlaying()) { // will always be true
+            final String[] instrumentInfos = player.sample1();
+
+            // Print details
+            System.out.print("Instruments chosen:  ");
+            for (int i = 0; i < instrumentInfos.length; i++) {
+                String info = instrumentInfos[i];
+                System.out.print(info);
+                if (i + 1 < instrumentInfos.length) {
+                    System.out.print(String.join("", Collections.nCopies(26 - info.length(), " ")));
+                }
+            }
+            System.out.println();
+
+            player.sample2();
+        }
+
+        // Close synthesizer
+        player.close();
+    }
+}
